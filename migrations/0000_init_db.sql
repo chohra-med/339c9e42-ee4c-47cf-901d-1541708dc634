cace02beb3395c7f301add4511f300453bfdda85
malik chohra <malik.chohra@docmorris.de>
2024-06-13 09:24:58 +0200

    clean code for push

--- migrations/0000_init_db.sql
+++ migrations/0000_init_db.sql
@@ -1,7 +1,8 @@
-CREATE TABLE `guestbook` (
+CREATE TABLE `user` (
 	`id` integer PRIMARY KEY NOT NULL,
 	`username` text NOT NULL,
 	`body` text NOT NULL,
+	`messages` text NOT NULL,
 	`created_at` integer DEFAULT (strftime('%s', 'now')),
 	`updated_at` integer DEFAULT (strftime('%s', 'now'))
 );
