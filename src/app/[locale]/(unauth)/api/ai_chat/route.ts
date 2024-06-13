7fbd970b58c46dd64a344a508c6aee47df306b26
malik chohra <malik.chohra@docmorris.de>
2024-06-13 21:06:11 +0200

    fix api call routes

--- src/app/[locale]/(unauth)/api/ai_chat/route.ts
+++ src/app/[locale]/(unauth)/api/ai_chat/route.ts
@@ -2,10 +2,10 @@ import { NextResponse } from 'next/server';
 
 import { logger } from '@/libs/Logger';
 
-export const POST = async (request: Request) => {
-  const apiKey = process.env.OPENAI_API_KEY;
+export const POST = async (req: Request) => {
+  const { messages } = await req.json();
 
-  const { messages } = request.body;
+  const apiKey = process.env.OPENAI_API_KEY;
   const url = 'https://api.openai.com/v1/chat/completions';
 
   const body = JSON.stringify({
@@ -23,13 +23,12 @@ export const POST = async (request: Request) => {
       },
       body,
     });
-    logger.info('Posted a message to OpenAI');
-
     const data = await response.json();
-    NextResponse.json({ data }, { status: 200 });
-  } catch (error) {
-    logger.error(error, 'An error occurred while updating a guestbook');
+    logger.info('A message was sent to the AI chatbot.');
 
+    return NextResponse.json({ data });
+  } catch (error) {
+    logger.error(error, 'An error occurred while posting a message');
     return NextResponse.json({}, { status: 500 });
   }
 };
--- src/utils/sendMessage.ts
+++ src/utils/sendMessage.ts
@@ -1,13 +1,14 @@
 import type { AiMessages } from '@/types/global';
 
 export const sendMessage = async (messages: AiMessages[]) => {
-  const response = await fetch('/api/createMessage', {
+  const response = await fetch(`/api/ai_chat`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ messages }),
   });
-  const data = await response.json();
+
+  const { data } = await response.json();
   return data;
 };
